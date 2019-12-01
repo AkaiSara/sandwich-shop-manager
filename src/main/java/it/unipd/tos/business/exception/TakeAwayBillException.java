0c7e33dacc6b2f017d19c209343f20442690e663
AkaiSara <sara.righetto@gmail.com>
2019-12-01 12:46:34 +0100

    Create classi

---
+++ src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// [Sara] [Righetto] [1174009]
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.tos.business.exception;
+
+public class TakeAwayBillException extends Throwable {
+    private String mex;
+
+    public TakeAwayBillException(String exc){
+        this.mex = exc;
+    }
+
+    public String getMessage(){
+        return this.mex;
+    }
+}
